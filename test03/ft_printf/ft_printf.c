774ff2445429cc70f1d6c35de5cb2a20ba41804e
김지우 <kimjiwoo8684@gmail.com>
2026-01-23 19:18:48 +0900

    ft_printf (fin)

--- test03/ft_printf/ft_printf.c
+++ test03/ft_printf/ft_printf.c
@@ -1,83 +1,74 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: clima-fr <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/06 15:30:40 by clima-fr          #+#    #+#             */
-/*   Updated: 2023/06/06 16:16:57 by clima-fr         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
 
-void	putstr(char *str, int *size)
+void putstr(char *str, int *size)
 {
-	if (!str)  
+	if(!str)
 		str = "(null)";
-	while (*str)
+	while(*str)
 	{
-		write(1, str, 1);
-		*size = *size + 1;
+		write(1,str, 1);
 		str++;
+		*size = *size + 1;
 	}
 }
 
-void	putnbr(long long int nbr, int base, int *size)
+void putnbr(long long int str, int bite, int *size)
 {
-	char	*digits = "0123456789abcdef";
+	char *c="0123456789abcdef";
 
-	if (nbr < 0)
+	if(str < 0)
 	{
+		write(1, "-",1);
+		str = str*-1;
 		*size = *size + 1;
-		write(1, "-", 1);
-		nbr = nbr * -1;
 	}
-	if (nbr >= base)
-		putnbr(nbr / base, base, size);
-	write (1, &digits[nbr % base], 1);
+	if (str >= bite)
+		putnbr(str / bite, bite, size);
+	write(1,&c[str%bite], 1);
 	*size = *size + 1;
 }
 
-int	ft_printf(const char *format, ...)
-{
-	int	size = 0;
-	va_list	pointer;
 
-	va_start (pointer, format);
-	while (*format)
+
+int ft_printf(const char*format, ...)
+{
+	int size = 0;
+	va_list form;
+
+	va_start(form, format);
+	while(*format)
 	{
-		if (*format == '%' && (*(format + 1) == 's' || *(format + 1) == 'd' || *(format + 1) == 'x'))
+		if(*format == '%' && ((*(format + 1) == 's') || (*(format + 1) == 'd') || (*(format + 1) == 'x')))
 		{
 			format++;
-			if (*format == 's')
-				putstr (va_arg(pointer, char *), &size);
-			else if (*format == 'd')
-				putnbr ((long long int)va_arg(pointer, int), 10, &size);
-			else if (*format == 'x')
-				putnbr ((long long int)va_arg(pointer, unsigned int), 16, &size);
-		}
+			if(*format == 's')
+				putstr(va_arg(form, char *), &size);
+			else if(*format == 'd')
+				putnbr((long long int)va_arg(form,int), 10,&size);
+			else if(*format == 'x')
+				putnbr((long long int)va_arg(form, unsigned int), 16,&size);
+		}	
 		else
 		{
-			write (1, format, 1);
+			write(1,format, 1);
 			size++;
 		}
 		format++;
 	}
-	va_end(pointer);
-	return (size);
+	va_end(form);
+	return(size);
 }
-// #include <stdio.h>
-// int main()
-// { 
-//     int return1 = ft_printf("Meu nome eh %s e eu tenho %d anos\n", "Clara", 23);
-//     int return2 = ft_printf("Testando o numero %d em hexadecimal: %x\n", 42, 42);
-//     printf("%d\n", return1);
-//     printf("%d\n", return2);
-//     return1 = printf("Meu nome eh %s e eu tenho %d anos\n", "Clara", 23);
-//     return2 = printf("Testando o numero %d em hexadecimal: %x\n", 42, 42);
-//     printf("%d\n", return1);
-//     printf("%d\n", return2);
-// }
+
+int main()
+{
+	int i = 0;
+	i = ft_printf("%d\n", 10);
+
+	printf("%d\n", i);
+	int j = 0;
+	j = printf("%s\n", "toto");
+	printf("%d\n", j);
+}
